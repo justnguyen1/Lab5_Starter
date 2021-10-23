c6e45dd1b44652804723c2bcfbe01e408ad4b09b
Justin Nguyen <justnguyen1@gmail.com>
2021-10-23 15:15:16 -0700

    fixed bug where voices weren't loading initially

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,32 +1,34 @@
 // explore.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  const speech = window.speechSynthesis;
-  const voices = speech.getVoices();
-  const dropdown = document.getElementById('voice-select');
-  loadVoices(dropdown, voices);
-  const pressToTalkButton = document.querySelector('button');
-  const textArea = document.querySelector('textarea');
-  const faceImage = document.querySelector('img');
-
-  pressToTalkButton.addEventListener('click', function() {
-    let textToSpeak = new SpeechSynthesisUtterance(textArea.value);
-    let selectedOption = dropdown.selectedOptions[0].getAttribute('data-name');
-    for (const voice of voices) {
-      if (voice.name === selectedOption) {
-        textToSpeak.voice = voice;
+  let speech = window.speechSynthesis;
+  let voices = [];
+  setTimeout(function() {
+    voices = speech.getVoices();
+    const dropdown = document.getElementById('voice-select');
+    loadVoices(dropdown, voices);
+    const pressToTalkButton = document.querySelector('button');
+    const textArea = document.querySelector('textarea');
+    const faceImage = document.querySelector('img');
+  
+    pressToTalkButton.addEventListener('click', function() {
+      let textToSpeak = new SpeechSynthesisUtterance(textArea.value);
+      let selectedOption = dropdown.selectedOptions[0].getAttribute('data-name');
+      for (const voice of voices) {
+        if (voice.name === selectedOption) {
+          textToSpeak.voice = voice;
+        }
       }
-    }
-    speech.speak(textToSpeak);
-    faceImage.setAttribute('src', 'assets/images/smiling-open.png');
-    faceImage.setAttribute('alt', 'Open mouth');
-    textToSpeak.onend = function() {
-      faceImage.setAttribute('src', 'assets/images/smiling.png');
-      faceImage.setAttribute('alt', 'Smiling face');
-    }
-  });
+      speech.speak(textToSpeak);
+      faceImage.setAttribute('src', 'assets/images/smiling-open.png');
+      faceImage.setAttribute('alt', 'Open mouth');
+      textToSpeak.onend = function() {
+        faceImage.setAttribute('src', 'assets/images/smiling.png');
+        faceImage.setAttribute('alt', 'Smiling face');
+      }
+    });
+  }, 50);
 }
 
 function loadVoices(dropdown, voices) {
